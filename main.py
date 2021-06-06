fb360eb44adbe5dabb2ede95c832ea8cf8cc204d
Joy <gajjarjoy4@gmail.com>
2021-06-06 10:06:41 +0530

    Text To Handwriting With pywhatkit

---
+++ main.py
@@ -0,0 +1,3 @@
+import pywhatkit
+
+pywhatkit.text_to_handwriting("This library can be installed by the pip command, open your command prompt and type in the following command...pip install pywhatkit")
